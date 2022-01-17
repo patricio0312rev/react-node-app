c3e7fe2904ae4b4188396283008e2fba6f942f7d
patricio0312rev <review0312oficial@gmail.com>
2022-01-16 23:51:37 -0500

    Configurando ruta GET para otros requests

--- server/index.js
+++ server/index.js
@@ -4,8 +4,14 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+// Handle GET requests to /api route
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
+});
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
 app.listen(PORT, () => {
